a6f6ebd8ad4b21bc5ae5f47f4a0de51802e48ccf
Martin Kudela <martin.kudelaa@gmail.com>
2025-01-08 16:00:31 +0100

    Fixed grid so now it will display only in right format every time (for now 16x16). Added button for change canvas implementation

--- scripts.js
+++ scripts.js
@@ -1,30 +1,23 @@
 const container = document.querySelector("#container");
 const divs = container.querySelectorAll("div");
 
-
-for(let i = 1; i <= 256; i++){
+function createGrid(gridRow){
+let gridSpace = gridRow * gridRow;
+for(let i = 1; i <= gridSpace; i++){
     const block = document.createElement("div");
+    block.style.width = `calc(100%/${gridRow})`;
+    block.style.height = `calc(100%/${gridRow})`;
     block.setAttribute("id", "square" + i);
     block.style.border = "2px solid black"
-    block.style.padding = "2.8%";
     block.addEventListener("mouseover", function(e) {
         block.style.backgroundColor = "red";
     });
     
     container.appendChild(block);
 }
+}
 
-
-divs.forEach((div) => {
-    div.addEventListener("mouseover", function(e) {
-        div.style.backgroundColor = "red";
-    });
-});
-
-
-
-
-
+createGrid(16);
 /*const container = document.querySelector("#container");
 const divs = document.querySelectorAll("div");
 
